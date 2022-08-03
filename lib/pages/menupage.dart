433a809e7d3d3e642e0ffe68e25401f28af15a5d
Roberto Villegas <hola@robertovillegas.com>
2022-08-03 15:55:40 -0500

    Designing the Menu Section

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_masters/datamodel.dart';
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
@@ -5,6 +6,81 @@ class MenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var product = Product(id: 1, name: "Dummy product", price: 1.25, image: "");
+    var product2 =
+        Product(id: 2, name: "Dummy product 2", price: 2.25, image: "");
+
+    return ListView(
+      children: [
+        ProductItem(
+          product: product,
+          onAdd: () {},
+        ),
+        ProductItem(
+          product: product2,
+          onAdd: () {},
+        ),
+      ],
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  final Function onAdd;
+
+  const ProductItem({
+    Key? key,
+    required this.product,
+    required this.onAdd,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Card(
+        elevation: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              "images/black_coffee.png",
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        product.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("\$${product.price}"),
+                      // Text(product.price.toString()),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      onAdd(product);
+                    },
+                    child: const Text("Add"),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
